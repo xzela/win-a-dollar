7021d86cd80802b504cf7ff24661c067f40256ff
Zeph Lafassett <zeph@lafassett.com>
2013-01-17 20:11:04 -0800

    classified this beeatch

--- winadollar.methods.php
+++ winadollar.methods.php
@@ -7,6 +7,17 @@ define("USER_SALT", 'five taste chicken');
 define("TEN_THOUNSAND", 101); //number of squares
 define("ONE_HUNDRED", 10); //square per row
 
+class Dollars {
+
+	public $conn;
+	public $id;
+
+	public function __construct() {
+		$this->conn = new Database();
+		$this->conn->open(); /// yeah open database connection!
+		$this->id = $this->hashUserSession(session_id());
+	}
+
 	/**
 	* returns the hashed session id, this is used
 	* for all players.
@@ -15,9 +26,10 @@ define("ONE_HUNDRED", 10); //square per row
 	*
 	* @return hash string
 	*/
-	function hashUserSession($id) {
-		return hash('sha256', USER_SALT . $id); //
+	public function hashUserSession($id) {
+		return hash('sha256', USER_SALT . $id);
 	}
+
 	/**
 	* tests the user to see if they already have a
 	* number[yes|no], We need to make sure the user is in
@@ -29,15 +41,12 @@ define("ONE_HUNDRED", 10); //square per row
 	*
 	* @return bool;
 	*/
-	function testCurrentUser() {
+	public function testCurrentUser() {
 		$b = false;
-		$id = hashUserSession(session_id());
-		$conn = new Database();
-		$conn->open();
 		$current_time = date('Y-m-d H:i:s');
 		$date = $new_time = date('Y-m-d H:i:s', strtotime($current_time . ' + 1 hour')); //
-		$sql = 'SELECT session_id FROM session_hash WHERE session_id = "' . $id . '" AND active = 1 AND entry_date < "' . $date . '" LIMIT 1';
-		$result = $conn->query($sql);
+		$sql = 'SELECT session_id FROM session_hash WHERE session_id = "' . $this->id . '" AND active = 1 AND entry_date < "' . $date . '" LIMIT 1';
+		$result = $this->conn->query($sql);
 		if(mysql_num_rows($result) > 0 ) {
 			$b = true;
 		}
@@ -49,14 +58,11 @@ define("ONE_HUNDRED", 10); //square per row
 	*
 	* @return null
 	*/
-	function createUserWinningNumber() {
-		$id = hashUserSession(session_id()); //hash the session_id
-		$hash = generateWinningNumber(); //create a winning number for this user
+	public function createUserWinningNumber() {
+		$hash = $this->generateWinningNumber(); //create a winning number for this user
 		$ip = $_SERVER['REMOTE_ADDR'];
-		$conn = new Database();
-		$conn->open();
-		$sql = 'INSERT INTO session_hash (session_id, number_hash, active, ip) VALUES ("' . $id . '","' . $hash . '",1,"' . $ip . '")';
-		$conn->query($sql);
+		$sql = 'INSERT INTO session_hash (session_id, number_hash, active, ip) VALUES ("' . $this->id . '","' . $hash . '",1,"' . $ip . '")'; //that's right, that's your ip right there.
+		$this->conn->query($sql);
 	}
 
 	/**
@@ -66,20 +72,18 @@ define("ONE_HUNDRED", 10); //square per row
 	*
 	* @return $hash [string]
 	*/
-	function getUsersWinningNumber() {
-		$id = hashUserSession(session_id()); //hash the session_id
-		$conn = new Database();
-		$conn->open();
+	public function getUsersWinningNumber() {
 		$current_time = date('Y-m-d H:i:s');
 		$date = $new_time = date('Y-m-d H:i:s', strtotime($current_time . ' + 1 hour')); //
-		$sql = 'SELECT number_hash FROM session_hash WHERE session_id = "' . $id . '" AND active = 1 AND entry_date < "' . $date . '" LIMIT 1';
-		$result = $conn->query($sql);
-		$hash = null;
+		$sql = 'SELECT number_hash FROM session_hash WHERE session_id = "' . $this->id . '" AND active = 1 AND entry_date < "' . $date . '" LIMIT 1';
+		$result = $this->conn->query($sql);
+		$hash = null; //set the hash to null
 		while($row = mysql_fetch_array($result)) {
 			$hash = $row['number_hash'];
 		}
 		return $hash;
 	}
+
 	/**
 	* this is the main cog
 	* this is called everytime the user clicks on a square
@@ -89,31 +93,31 @@ define("ONE_HUNDRED", 10); //square per row
 	*
 	* @return $b [bool];
 	*/
-	function checkUsersWinningNumber($hash) {
+	public function checkUsersWinningNumber($hash) {
 		$b = false;
-		$id = hashUserSession(session_id()); //hash the session_id
-		$conn = new Database();
+		$hash = mysql_real_escape_string($hash);
 		$current_time = date('Y-m-d H:i:s');
 		$date = $new_time = date('Y-m-d H:i:s', strtotime($current_time . ' + 1 hour')); //
-		$sql = 'SELECT number_hash FROM session_hash WHERE session_id = "' . $id . '" AND number_hash = "' . $hash . '" AND active = 1 AND entry_date < "' . $date . '" LIMIT 1';
-		$result = $conn->query($sql);
+		$sql = 'SELECT number_hash FROM session_hash WHERE session_id = "' . $this->id . '" AND number_hash = "' . $hash . '" AND active = 1 AND entry_date < "' . $date . '" LIMIT 1';
+		$result = $this->conn->query($sql);
 		if(mysql_num_rows($result) > 0) {
 			$b = true;
-			$won_query = 'UPDATE session_hash SET active = 0, winner = 1 WHERE session_id = "' . $id . '" AND number_hash = "' . $hash . '" LIMIT 1';
-			$conn->query($won_query);
+			$won_query = 'UPDATE session_hash SET active = 0, winner = 1 WHERE session_id = "' . $this->id . '" AND number_hash = "' . $hash . '" LIMIT 1';
+			$this->conn->query($won_query);
 		}
 		else {
-			$lost_query = 'UPDATE session_hash SET active = 0, winner = 0 WHERE session_id = "' . $id . '" AND winner = 0';
-			$conn->query($lost_query);
+			$lost_query = 'UPDATE session_hash SET active = 0, winner = 0 WHERE session_id = "' . $this->id . '" AND winner = 0';
+			$this->conn->query($lost_query);
 		}
-		createUserWinningNumber();
-
+		$this->createUserWinningNumber();
 		return $b;
 	}
 
-	function generateWinningNumber() {
+	public function generateWinningNumber() {
 		$num = rand(1, TEN_THOUNSAND);
 		$number = hash('sha256', SALT . $num);
 		return $number;
 	}
+
+}
 ?>
